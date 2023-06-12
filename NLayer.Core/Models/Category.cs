60e60c9b221442aaafbb75924efc98e990633051
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-13 00:04:28 +0300

    DTOs

--- NLayer.Core/Models/Category.cs
+++ NLayer.Core/Models/Category.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace NLayer.Core
+
+namespace NLayer.Core.Models
 {
     public class Category : BaseEntity
     {
