35a44e659accd7bb0e3bab90f41d1f66e41b88b9
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-13 00:56:39 +0300

    CustomResponseDto

---
+++ NLayer.Core/DTOs/NoContentDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class NoContentDTO
+    {
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public List<string> Errors { get; set; }
+
+        public static NoContentDTO Success(int statusCode)
+        {
+            return new NoContentDTO { StatusCode = statusCode };
+
+        }
+
+        public static NoContentDTO Fail(int statusCode, List<string> errors)
+        {
+            return new NoContentDTO { StatusCode = statusCode, Errors = errors };
+        }
+
+        public static NoContentDTO Fail(int statusCode, string error)
+        {
+            return new NoContentDTO { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+    }
+}
