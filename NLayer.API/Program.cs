ab380d6aabc391c196bf99b07131dca392f6bf4c
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-15 18:12:37 +0300

    CachingService-1

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -33,6 +33,8 @@ builder.Services.Configure<ApiBehaviorOptions>(opt =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 
